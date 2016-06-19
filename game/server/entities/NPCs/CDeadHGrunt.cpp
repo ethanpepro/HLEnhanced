ec0235b9e08a57cc4888a69aefd26adf6a97dafd
Sam <duo929@hotmail.com>
2016-06-19 17:27:49 +0200

    Move HGrunt to game/ dir
    Moved the HGrunt entities to the game/ directory

---
+++ game/server/entities/NPCs/CDeadHGrunt.cpp
@@ -0,0 +1,93 @@
+/***
+*
+*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*	All Rights Reserved.
+*
+*   This source code contains proprietary and confidential information of
+*   Valve LLC and its suppliers.  Access to this code is restricted to
+*   persons who have executed a written SDK license with Valve.  Any access,
+*   use or distribution of this code by or to any unlicensed person is illegal.
+*
+****/
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "CSquadMonster.h"
+#include "Monsters.h"
+
+#include "CHGrunt.h"
+
+#include "CDeadHGrunt.h"
+
+char *CDeadHGrunt::m_szPoses[] = { "deadstomach", "deadside", "deadsitting" };
+
+LINK_ENTITY_TO_CLASS( monster_hgrunt_dead, CDeadHGrunt );
+
+//=========================================================
+// ********** DeadHGrunt SPAWN **********
+//=========================================================
+void CDeadHGrunt::Spawn( void )
+{
+	PRECACHE_MODEL( "models/hgrunt.mdl" );
+	SET_MODEL( ENT( pev ), "models/hgrunt.mdl" );
+
+	pev->effects = 0;
+	pev->yaw_speed = 8;
+	pev->sequence = 0;
+	m_bloodColor = BLOOD_COLOR_RED;
+
+	pev->sequence = LookupSequence( m_szPoses[ m_iPose ] );
+
+	if( pev->sequence == -1 )
+	{
+		ALERT( at_console, "Dead hgrunt with bad pose\n" );
+	}
+
+	// Corpses have less health
+	pev->health = 8;
+
+	// map old bodies onto new bodies
+	switch( pev->body )
+	{
+	case 0: // Grunt with Gun
+		pev->body = 0;
+		pev->skin = 0;
+		SetBodygroup( HEAD_GROUP, HEAD_GRUNT );
+		SetBodygroup( GUN_GROUP, GUN_MP5 );
+		break;
+	case 1: // Commander with Gun
+		pev->body = 0;
+		pev->skin = 0;
+		SetBodygroup( HEAD_GROUP, HEAD_COMMANDER );
+		SetBodygroup( GUN_GROUP, GUN_MP5 );
+		break;
+	case 2: // Grunt no Gun
+		pev->body = 0;
+		pev->skin = 0;
+		SetBodygroup( HEAD_GROUP, HEAD_GRUNT );
+		SetBodygroup( GUN_GROUP, GUN_NONE );
+		break;
+	case 3: // Commander no Gun
+		pev->body = 0;
+		pev->skin = 0;
+		SetBodygroup( HEAD_GROUP, HEAD_COMMANDER );
+		SetBodygroup( GUN_GROUP, GUN_NONE );
+		break;
+	}
+
+	MonsterInitDead();
+}
+
+void CDeadHGrunt::KeyValue( KeyValueData *pkvd )
+{
+	if( FStrEq( pkvd->szKeyName, "pose" ) )
+	{
+		m_iPose = atoi( pkvd->szValue );
+		pkvd->fHandled = true;
+	}
+	else
+		CBaseMonster::KeyValue( pkvd );
+}
--- game/server/entities/NPCs/CHGrunt.cpp
+++ game/server/entities/NPCs/CHGrunt.cpp
@@ -15,178 +15,23 @@
 //=========================================================
 // hgrunt
 //=========================================================
+#include "extdll.h"
+#include "CPlane.h"
+#include "util.h"
+#include "cbase.h"
+#include "Monsters.h"
+#include "Schedule.h"
+#include "animation.h"
+#include "CSquadMonster.h"
+#include "Weapons.h"
+#include "CTalkMonster.h"
+#include "entities/CSoundEnt.h"
 
-//=========================================================
-// Hit groups!	
-//=========================================================
-/*
-
-  1 - Head
-  2 - Stomach
-  3 - Gun
-
-*/
-
-
-#include	"extdll.h"
-#include	"CPlane.h"
-#include	"util.h"
-#include	"cbase.h"
-#include	"entities/NPCs/Monsters.h"
-#include	"entities/NPCs/Schedule.h"
-#include	"animation.h"
-#include	"entities/NPCs/CSquadMonster.h"
-#include	"Weapons.h"
-#include	"entities/NPCs/CTalkMonster.h"
-#include	"entities/CSoundEnt.h"
-#include	"Effects.h"
-#include	"customentity.h"
+#include "CHGrunt.h"
 
 int g_fGruntQuestion;				// true if an idle grunt asked a question. Cleared when someone answers.
 
 extern DLL_GLOBAL int		g_iSkillLevel;
-
-//=========================================================
-// monster-specific DEFINE's
-//=========================================================
-#define	GRUNT_CLIP_SIZE					36 // how many bullets in a clip? - NOTE: 3 round burst sound, so keep as 3 * x!
-#define GRUNT_VOL						0.35		// volume of grunt sounds
-#define GRUNT_ATTN						ATTN_NORM	// attenutation of grunt sentences
-#define HGRUNT_LIMP_HEALTH				20
-#define HGRUNT_DMG_HEADSHOT				( DMG_BULLET | DMG_CLUB )	// damage types that can kill a grunt with a single headshot.
-#define HGRUNT_NUM_HEADS				2 // how many grunt heads are there? 
-#define HGRUNT_MINIMUM_HEADSHOT_DAMAGE	15 // must do at least this much damage in one shot to head to score a headshot kill
-#define	HGRUNT_SENTENCE_VOLUME			(float)0.35 // volume of grunt sentences
-
-#define HGRUNT_9MMAR				( 1 << 0)
-#define HGRUNT_HANDGRENADE			( 1 << 1)
-#define HGRUNT_GRENADELAUNCHER		( 1 << 2)
-#define HGRUNT_SHOTGUN				( 1 << 3)
-
-#define HEAD_GROUP					1
-#define HEAD_GRUNT					0
-#define HEAD_COMMANDER				1
-#define HEAD_SHOTGUN				2
-#define HEAD_M203					3
-#define GUN_GROUP					2
-#define GUN_MP5						0
-#define GUN_SHOTGUN					1
-#define GUN_NONE					2
-
-//=========================================================
-// Monster's Anim Events Go Here
-//=========================================================
-#define		HGRUNT_AE_RELOAD		( 2 )
-#define		HGRUNT_AE_KICK			( 3 )
-#define		HGRUNT_AE_BURST1		( 4 )
-#define		HGRUNT_AE_BURST2		( 5 ) 
-#define		HGRUNT_AE_BURST3		( 6 ) 
-#define		HGRUNT_AE_GREN_TOSS		( 7 )
-#define		HGRUNT_AE_GREN_LAUNCH	( 8 )
-#define		HGRUNT_AE_GREN_DROP		( 9 )
-#define		HGRUNT_AE_CAUGHT_ENEMY	( 10) // grunt established sight with an enemy (player only) that had previously eluded the squad.
-#define		HGRUNT_AE_DROP_GUN		( 11) // grunt (probably dead) is dropping his mp5.
-
-//=========================================================
-// monster-specific schedule types
-//=========================================================
-enum
-{
-	SCHED_GRUNT_SUPPRESS = LAST_COMMON_SCHEDULE + 1,
-	SCHED_GRUNT_ESTABLISH_LINE_OF_FIRE,// move to a location to set up an attack against the enemy. (usually when a friendly is in the way).
-	SCHED_GRUNT_COVER_AND_RELOAD,
-	SCHED_GRUNT_SWEEP,
-	SCHED_GRUNT_FOUND_ENEMY,
-	SCHED_GRUNT_REPEL,
-	SCHED_GRUNT_REPEL_ATTACK,
-	SCHED_GRUNT_REPEL_LAND,
-	SCHED_GRUNT_WAIT_FACE_ENEMY,
-	SCHED_GRUNT_TAKECOVER_FAILED,// special schedule type that forces analysis of conditions and picks the best possible schedule to recover from this type of failure.
-	SCHED_GRUNT_ELOF_FAIL,
-};
-
-//=========================================================
-// monster-specific tasks
-//=========================================================
-enum 
-{
-	TASK_GRUNT_FACE_TOSS_DIR = LAST_COMMON_TASK + 1,
-	TASK_GRUNT_SPEAK_SENTENCE,
-	TASK_GRUNT_CHECK_FIRE,
-};
-
-//=========================================================
-// monster-specific conditions
-//=========================================================
-#define bits_COND_GRUNT_NOFIRE	( bits_COND_SPECIAL1 )
-
-class CHGrunt : public CSquadMonster
-{
-public:
-	DECLARE_CLASS( CHGrunt, CSquadMonster );
-	DECLARE_DATADESC();
-
-	void Spawn( void ) override;
-	void Precache( void ) override;
-	void SetYawSpeed ( void ) override;
-	int  Classify ( void ) override;
-	int ISoundMask ( void ) override;
-	void HandleAnimEvent( MonsterEvent_t *pEvent ) override;
-	bool FCanCheckAttacks() const override;
-	bool CheckMeleeAttack1 ( float flDot, float flDist ) override;
-	bool CheckRangeAttack1 ( float flDot, float flDist ) override;
-	bool CheckRangeAttack2 ( float flDot, float flDist ) override;
-	void CheckAmmo ( void );
-	void SetActivity ( Activity NewActivity ) override;
-	void StartTask ( Task_t *pTask ) override;
-	void RunTask ( Task_t *pTask ) override;
-	void DeathSound( void ) override;
-	void PainSound( void ) override;
-	void IdleSound ( void ) override;
-	Vector GetGunPosition( void ) override;
-	void Shoot ( void );
-	void Shotgun ( void );
-	void PrescheduleThink ( void ) override;
-	void GibMonster( void ) override;
-	void SpeakSentence( void );
-	
-	CBaseEntity	*Kick( void );
-	Schedule_t	*GetSchedule( void ) override;
-	Schedule_t  *GetScheduleOfType ( int Type ) override;
-	void TraceAttack( entvars_t *pevAttacker, float flDamage, Vector vecDir, TraceResult *ptr, int bitsDamageType) override;
-	int TakeDamage( CBaseEntity* pInflictor, CBaseEntity* pAttacker, float flDamage, int bitsDamageType ) override;
-
-	int IRelationship ( CBaseEntity *pTarget ) override;
-
-	bool FOkToSpeak() const;
-	void JustSpoke( void );
-
-	CUSTOM_SCHEDULES;
-
-	// checking the feasibility of a grenade toss is kind of costly, so we do it every couple of seconds,
-	// not every server frame.
-	float m_flNextGrenadeCheck;
-	float m_flNextPainTime;
-	float m_flLastEnemySightTime;
-
-	Vector	m_vecTossVelocity;
-
-	bool	m_fThrowGrenade;
-	bool	m_fStanding;
-	bool	m_fFirstEncounter;// only put on the handsign show in the squad's first encounter.
-	int		m_cClipSize;
-
-	int m_voicePitch;
-
-	int		m_iBrassShell;
-	int		m_iShotgunShell;
-
-	int		m_iSentence;
-
-	static const char *pGruntSentences[];
-};
-
-LINK_ENTITY_TO_CLASS( monster_human_grunt, CHGrunt );
 
 BEGIN_DATADESC(	CHGrunt )
 	DEFINE_FIELD( m_flNextGrenadeCheck, FIELD_TIME ),
@@ -203,6 +48,8 @@ BEGIN_DATADESC(	CHGrunt )
 	DEFINE_FIELD( m_iSentence, FIELD_INTEGER ),
 END_DATADESC()
 
+LINK_ENTITY_TO_CLASS( monster_human_grunt, CHGrunt );
+
 const char *CHGrunt::pGruntSentences[] = 
 {
 	"HG_GREN", // grenade scared grunt
@@ -213,18 +60,6 @@ const char *CHGrunt::pGruntSentences[] =
 	"HG_CHARGE",  // running out to get the enemy
 	"HG_TAUNT", // say rude things
 };
-
-enum
-{
-	HGRUNT_SENT_NONE = -1,
-	HGRUNT_SENT_GREN = 0,
-	HGRUNT_SENT_ALERT,
-	HGRUNT_SENT_MONSTER,
-	HGRUNT_SENT_COVER,
-	HGRUNT_SENT_THROW,
-	HGRUNT_SENT_CHARGE,
-	HGRUNT_SENT_TAUNT,
-} HGRUNT_SENTENCE_TYPES;
 
 //=========================================================
 // Speak Sentence - say your cued up sentence.
@@ -2365,153 +2200,4 @@ Schedule_t* CHGrunt :: GetScheduleOfType ( int Type )
 			return CSquadMonster :: GetScheduleOfType ( Type );
 		}
 	}
-}
-
-
-//=========================================================
-// CHGruntRepel - when triggered, spawns a monster_human_grunt
-// repelling down a line.
-//=========================================================
-
-class CHGruntRepel : public CBaseMonster
-{
-public:
-	DECLARE_CLASS( CHGruntRepel, CBaseMonster );
-
-	void Spawn( void ) override;
-	void Precache( void ) override;
-	void EXPORT RepelUse ( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value );
-	int m_iSpriteTexture;	// Don't save, precache
-};
-
-LINK_ENTITY_TO_CLASS( monster_grunt_repel, CHGruntRepel );
-
-void CHGruntRepel::Spawn( void )
-{
-	Precache( );
-	pev->solid = SOLID_NOT;
-
-	SetUse( &CHGruntRepel::RepelUse );
-}
-
-void CHGruntRepel::Precache( void )
-{
-	UTIL_PrecacheOther( "monster_human_grunt" );
-	m_iSpriteTexture = PRECACHE_MODEL( "sprites/rope.spr" );
-}
-
-void CHGruntRepel::RepelUse ( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value )
-{
-	TraceResult tr;
-	UTIL_TraceLine( pev->origin, pev->origin + Vector( 0, 0, -4096.0), dont_ignore_monsters, ENT(pev), &tr);
-	/*
-	if ( tr.pHit && Instance( tr.pHit )->pev->solid != SOLID_BSP) 
-		return NULL;
-	*/
-
-	CBaseEntity *pEntity = Create( "monster_human_grunt", pev->origin, pev->angles );
-	CBaseMonster *pGrunt = pEntity->MyMonsterPointer( );
-	pGrunt->pev->movetype = MOVETYPE_FLY;
-	pGrunt->pev->velocity = Vector( 0, 0, RANDOM_FLOAT( -196, -128 ) );
-	pGrunt->SetActivity( ACT_GLIDE );
-	// UNDONE: position?
-	pGrunt->m_vecLastPosition = tr.vecEndPos;
-
-	CBeam *pBeam = CBeam::BeamCreate( "sprites/rope.spr", 10 );
-	pBeam->PointEntInit( pev->origin + Vector(0,0,112), pGrunt->entindex() );
-	pBeam->SetFlags( BEAM_FSOLID );
-	pBeam->SetColor( 255, 255, 255 );
-	pBeam->SetThink( &CBeam::SUB_Remove );
-	pBeam->pev->nextthink = gpGlobals->time + -4096.0 * tr.flFraction / pGrunt->pev->velocity.z + 0.5;
-
-	UTIL_Remove( this );
-}
-
-
-
-//=========================================================
-// DEAD HGRUNT PROP
-//=========================================================
-class CDeadHGrunt : public CBaseMonster
-{
-public:
-	DECLARE_CLASS( CDeadHGrunt, CBaseMonster );
-
-	void Spawn( void ) override;
-	int	Classify ( void ) override { return	CLASS_HUMAN_MILITARY; }
-
-	void KeyValue( KeyValueData *pkvd ) override;
-
-	int	m_iPose;// which sequence to display	-- temporary, don't need to save
-	static char *m_szPoses[3];
-};
-
-char *CDeadHGrunt::m_szPoses[] = { "deadstomach", "deadside", "deadsitting" };
-
-void CDeadHGrunt::KeyValue( KeyValueData *pkvd )
-{
-	if (FStrEq(pkvd->szKeyName, "pose"))
-	{
-		m_iPose = atoi(pkvd->szValue);
-		pkvd->fHandled = true;
-	}
-	else 
-		CBaseMonster::KeyValue( pkvd );
-}
-
-LINK_ENTITY_TO_CLASS( monster_hgrunt_dead, CDeadHGrunt );
-
-//=========================================================
-// ********** DeadHGrunt SPAWN **********
-//=========================================================
-void CDeadHGrunt :: Spawn( void )
-{
-	PRECACHE_MODEL("models/hgrunt.mdl");
-	SET_MODEL(ENT(pev), "models/hgrunt.mdl");
-
-	pev->effects		= 0;
-	pev->yaw_speed		= 8;
-	pev->sequence		= 0;
-	m_bloodColor		= BLOOD_COLOR_RED;
-
-	pev->sequence = LookupSequence( m_szPoses[m_iPose] );
-
-	if (pev->sequence == -1)
-	{
-		ALERT ( at_console, "Dead hgrunt with bad pose\n" );
-	}
-
-	// Corpses have less health
-	pev->health			= 8;
-
-	// map old bodies onto new bodies
-	switch( pev->body )
-	{
-	case 0: // Grunt with Gun
-		pev->body = 0;
-		pev->skin = 0;
-		SetBodygroup( HEAD_GROUP, HEAD_GRUNT );
-		SetBodygroup( GUN_GROUP, GUN_MP5 );
-		break;
-	case 1: // Commander with Gun
-		pev->body = 0;
-		pev->skin = 0;
-		SetBodygroup( HEAD_GROUP, HEAD_COMMANDER );
-		SetBodygroup( GUN_GROUP, GUN_MP5 );
-		break;
-	case 2: // Grunt no Gun
-		pev->body = 0;
-		pev->skin = 0;
-		SetBodygroup( HEAD_GROUP, HEAD_GRUNT );
-		SetBodygroup( GUN_GROUP, GUN_NONE );
-		break;
-	case 3: // Commander no Gun
-		pev->body = 0;
-		pev->skin = 0;
-		SetBodygroup( HEAD_GROUP, HEAD_COMMANDER );
-		SetBodygroup( GUN_GROUP, GUN_NONE );
-		break;
-	}
-
-	MonsterInitDead();
 }
