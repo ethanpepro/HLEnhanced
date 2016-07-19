044cebd3e7548d90841f720e1a171ec65611debe
Sam <duo929@hotmail.com>
2016-07-19 14:29:17 +0200

    Move AutoAim constants, add new constants

--- game/shared/GameConstants.h
+++ game/shared/GameConstants.h
@@ -52,4 +52,36 @@
 */
 #define PLAYER_STEP_SOUND_SPEED 220
 
+/**
+*	@defgroup AutoAimDegrees AutoAim Degrees
+*
+*	AutoAim degrees for use in weapons code.
+*
+*	Calculated as follows:
+*	UTIL_MakeVectors( Vector( 0, 0, 0 ) );
+*
+*	AngleVectors( Vector( degrees, 0, 0 ), outVector, nullptr, nullptr );
+*
+*	double dot = fabs( DotProduct( outVector, gpGlobals->v_right ) )
+*		+ fabs( DotProduct( outVector, gpGlobals->v_up ) ) * 0.5;
+*
+*	const double dot2 = dot * 2;
+*
+*	Use doubles when calculating for inproved accuracy.
+*
+*	@{
+*/
+#define AUTOAIM_1DEGREE		0.01745240643728
+#define AUTOAIM_2DEGREES	0.03489949670250
+#define AUTOAIM_3DEGREES	0.05233595624294
+#define AUTOAIM_4DEGREES	0.06975647374413
+#define AUTOAIM_5DEGREES	0.08715574274766
+#define AUTOAIM_6DEGREES	0.10452846326765
+#define AUTOAIM_7DEGREES	0.12186934340515
+#define AUTOAIM_8DEGREES	0.13917310096007
+#define AUTOAIM_9DEGREES	0.15643446504023
+#define AUTOAIM_10DEGREES	0.17364817766690
+
+/** @} */
+
 #endif //GAME_SHARED_GAMECONSTANTS_H
