f77be1424f982e42062515409be2a6ad540c133d
Sam <duo929@hotmail.com>
2016-07-21 22:21:24 +0200

    Add CBufferReader, rewrite CBufferWriter

--- common/parsemsg.cpp
+++ common/parsemsg.cpp
@@ -174,86 +174,222 @@ float READ_HIRESANGLE( void )
 }
 
 //--------------------------------------------------------------------------------------------------------------
-BufferWriter::BufferWriter()
+CBaseBuffer::CBaseBuffer()
 {
 	Init( nullptr, 0 );
 }
 
 //--------------------------------------------------------------------------------------------------------------
-BufferWriter::BufferWriter( unsigned char *buffer, int bufferLen )
+CBaseBuffer::CBaseBuffer( unsigned char* pBuffer, const size_t uiBufferSize )
 {
-	Init( buffer, bufferLen );
+	Init( pBuffer, uiBufferSize );
 }
 
 //--------------------------------------------------------------------------------------------------------------
-void BufferWriter::Init( unsigned char *buffer, int bufferLen )
+void CBaseBuffer::Init( unsigned char* pBuffer, const size_t uiBufferSize )
 {
-	m_overflow = false;
-	m_buffer = buffer;
-	m_remaining = bufferLen;
-	m_overallLength = bufferLen;
+	m_pBuffer = pBuffer;
+	m_uiBufferSize = uiBufferSize;
+	m_uiPosition = 0;
+	m_bOverflow = false;
+}
+
+bool CBaseBuffer::CheckOverflow( const size_t uiSize ) const
+{
+	return m_uiPosition + uiSize > m_uiBufferSize;
+}
+
+bool CBaseBuffer::Overflow( const size_t uiSize )
+{
+	if( CheckOverflow( uiSize ) )
+	{
+		m_bOverflow = true;
+
+		return true;
+	}
+
+	return false;
+}
+
+int CBufferReader::ReadChar()
+{
+	static_assert( sizeof( signed char ) == 1, "BufferReader::ReadChar: sizeof( signed char ) must be 1!" );
+
+	if( Overflow( 1 ) )
+	{
+		return -1;
+	}
+
+	int c = ( signed char ) m_pBuffer[ m_uiPosition++ ];
+
+	return c;
+}
+
+int CBufferReader::ReadByte()
+{
+	static_assert( sizeof( unsigned char ) == 1, "BufferReader::ReadByte: sizeof( unsigned char ) must be 1!" );
+
+	if( Overflow( 1 ) )
+	{
+		return -1;
+	}
+
+	int c = ( unsigned char ) m_pBuffer[ m_uiPosition++ ];
+
+	return c;
+}
+
+int CBufferReader::ReadShort()
+{
+	static_assert( sizeof( short ) == 2, "BufferReader::ReadShort: sizeof( short ) must be 2!" );
+
+	if( Overflow( 2 ) )
+	{
+		return -1;
+	}
+
+	int c = ( short ) ( m_pBuffer[ m_uiPosition ] + ( m_pBuffer[ m_uiPosition + 1 ] << 8 ) );
+
+	m_uiPosition += 2;
+
+	return c;
+}
+
+int CBufferReader::ReadWord()
+{
+	return READ_SHORT();
+}
+
+int CBufferReader::ReadLong()
+{
+	static_assert( sizeof( int ) == 4, "BufferReader::ReadLong: sizeof( int ) must be 4!" );
+
+	if( Overflow( 4 ) )
+	{
+		return -1;
+	}
+
+	int c = m_pBuffer[ m_uiPosition ] + ( m_pBuffer[ m_uiPosition + 1 ] << 8 ) + ( m_pBuffer[ m_uiPosition + 2 ] << 16 ) + ( m_pBuffer[ m_uiPosition + 3 ] << 24 );
+
+	m_uiPosition += 4;
+
+	return c;
+}
+
+float CBufferReader::ReadFloat()
+{
+	static_assert( sizeof( float ) == 4, "BufferReader::ReadFloat: sizeof( float ) must be 4!" );
+
+	if( Overflow( 4 ) )
+	{
+		return -1;
+	}
+
+	union
+	{
+		byte    b[ 4 ];
+		float   f;
+		int     l;
+	} dat;
+
+	dat.b[ 0 ] = m_pBuffer[ m_uiPosition ];
+	dat.b[ 1 ] = m_pBuffer[ m_uiPosition + 1 ];
+	dat.b[ 2 ] = m_pBuffer[ m_uiPosition + 2 ];
+	dat.b[ 3 ] = m_pBuffer[ m_uiPosition + 3 ];
+	m_uiPosition += 4;
+
+	return dat.f;
+}
+
+char* CBufferReader::ReadString()
+{
+	static char string[ 2048 ];
+	int c;
+
+	string[ 0 ] = '\0';
+
+	size_t l = 0;
+	do
+	{
+		if( m_uiPosition + 1 > m_uiBufferSize )
+			break; // no more characters
+
+		c = ReadChar();
+		if( c == -1 || c == '\0' )
+			break;
+		string[ l ] = c;
+		l++;
+	}
+	while( l < sizeof( string ) - 1 );
+
+	string[ l ] = '\0';
+
+	return string;
+}
+
+float CBufferReader::ReadCoord()
+{
+	return ( float ) ( ReadShort() * ( 1.0 / 8 ) );
+}
+
+float CBufferReader::ReadAngle()
+{
+	return ( float ) ( ReadChar() * ( 360.0 / 256 ) );
+}
+
+float CBufferReader::ReadHiResAngle()
+{
+	return ( float ) ( ReadShort() * ( 360.0 / 65536 ) );
 }
 
 //--------------------------------------------------------------------------------------------------------------
 void BufferWriter::WriteByte( unsigned char data )
 {
-	if (!m_buffer || !m_remaining)
+	if (!m_pBuffer || !GetSpaceLeft())
 	{
-		m_overflow = true;
+		m_bOverflow = true;
 		return;
 	}
 
-	*m_buffer = data;
-	++m_buffer;
-	--m_remaining;
+	m_pBuffer[ m_uiPosition++ ] = data;
 }
 
 //--------------------------------------------------------------------------------------------------------------
 void BufferWriter::WriteLong( int data )
 {
-	if (!m_buffer || m_remaining < 4)
+	if (!m_pBuffer || GetSpaceLeft() < 4)
 	{
-		m_overflow = true;
+		m_bOverflow = true;
 		return;
 	}
 
-	m_buffer[0] = data&0xff;
-	m_buffer[1] = (data>>8)&0xff;
-	m_buffer[2] = (data>>16)&0xff;
-	m_buffer[3] = data>>24;
-	m_buffer += 4;
-	m_remaining -= 4;
+	m_pBuffer[ m_uiPosition ] = data&0xff;
+	m_pBuffer[ m_uiPosition + 1 ] = (data>>8)&0xff;
+	m_pBuffer[ m_uiPosition + 2 ] = (data>>16)&0xff;
+	m_pBuffer[ m_uiPosition + 3 ] = data>>24;
+	m_uiPosition += 4;
 }
 
 //--------------------------------------------------------------------------------------------------------------
-void BufferWriter::WriteString( const char *str )
+void BufferWriter::WriteString( const char* pszString )
 {
-	if (!m_buffer || !m_remaining)
+	if( !m_pBuffer || !GetSpaceLeft() )
 	{
-		m_overflow = true;
+		m_bOverflow = true;
 		return;
 	}
 
-	if (!str)
-		str = "";
+	if( !pszString )
+		pszString = "";
 
-	int len = strlen(str)+1;
-	if ( len > m_remaining )
+	size_t len = strlen( pszString ) + 1;
+	if ( len > GetSpaceLeft() )
 	{
-		m_overflow = true;
-		str = "";
+		m_bOverflow = true;
+		pszString = "";
 		len = 1;
 	}
 
-	strcpy((char *)m_buffer, str);
-	m_remaining -= len;
-	m_buffer += len;
+	strcpy( (char *) m_pBuffer, pszString );
+	m_uiPosition += len;
 }
-
-//--------------------------------------------------------------------------------------------------------------
-int BufferWriter::GetSpaceUsed()
-{
-	return m_overallLength - m_remaining;
-}
-
-//--------------------------------------------------------------------------------------------------------------
--- common/parsemsg.h
+++ common/parsemsg.h
@@ -35,29 +35,159 @@ float READ_ANGLE( void );
 float READ_HIRESANGLE( void );
 bool READ_OK( void );
 
-//--------------------------------------------------------------------------------------------------------------
-class BufferWriter
+/**
+*	Base class for byte buffers.
+*/
+class CBaseBuffer
 {
 public:
-	BufferWriter();
-	BufferWriter( unsigned char *buffer, int bufferLen );
-	void Init( unsigned char *buffer, int bufferLen );
+	/**
+	*	Initializes the buffer with no backing buffer to use.
+	*/
+	CBaseBuffer();
 
-	void WriteByte( unsigned char data );
-	void WriteLong( int data );
-	void WriteString( const char *str );
+	/**
+	*	Initializes the buffer with the given backing buffer.
+	*	@param pBuffer Buffer to use.
+	*	@param uiBufferSize Size of the buffer, in bytes.
+	*/
+	CBaseBuffer( unsigned char* pBuffer, const size_t uiBufferSize );
 
-	bool HasOverflowed();
-	int GetSpaceUsed();
+	/**
+	*	Constructor for engine buffer data.
+	*/
+	CBaseBuffer( void* pBuffer, const int iBufferSize )
+		: CBaseBuffer( reinterpret_cast<unsigned char*>( pBuffer ), static_cast<size_t>( iBufferSize ) )
+	{
+	}
+
+	/**
+	*	@copydoc CBaseBuffer( unsigned char* pBuffer, const size_t uiBufferSize )
+	*/
+	void Init( unsigned char* pBuffer, const size_t uiBufferSize );
+
+	/**
+	*	@return Whether this buffer has overflowed.
+	*/
+	bool HasOverflowed() const { return m_bOverflow; }
+
+	/**
+	*	@return The amount of space in the buffer that has been read or written.
+	*/
+	size_t GetSpaceUsed() const { return m_uiPosition; }
+
+	/**
+	*	@return The amount of space that is left to be read or written.
+	*/
+	size_t GetSpaceLeft() const { return m_uiBufferSize - m_uiPosition; }
+
+	/**
+	*	Checks whether reading or writing uiSize bytes would cause an overflow.
+	*	@param uiSize Number of bytes.
+	*	@return true if an overflow would occur, false otherwise.
+	*/
+	bool CheckOverflow( const size_t uiSize ) const;
+
+	/**
+	*	Checks whether reading or writing uiSize bytes would cause an overflow. If so, sets the overflow flag.
+	*	@param uiSize Number of bytes.
+	*	@return true if an overflow has occurred, false otherwise.
+	*/
+	bool Overflow( const size_t uiSize );
 
 protected:
-	unsigned char *m_buffer;
-	int m_remaining;
-	bool m_overflow;
-	int m_overallLength;
+	unsigned char* m_pBuffer;
+	size_t m_uiBufferSize;
+	size_t m_uiPosition;
+	bool m_bOverflow;
+
+private:
+	CBaseBuffer( const CBaseBuffer& ) = delete;
+	CBaseBuffer& operator=( const CBaseBuffer& ) = delete;
 };
 
-//--------------------------------------------------------------------------------------------------------------
+/**
+*	Read buffer.
+*/
+class CBufferReader : public CBaseBuffer
+{
+public:
+	using CBaseBuffer::CBaseBuffer;
+
+	/**
+	*	Reads a signed char.
+	*/
+	int ReadChar();
+
+	/**
+	*	Reads an unsigned char.
+	*/
+	int ReadByte();
+
+	/**
+	*	Reads a signed short.
+	*/
+	int ReadShort();
+
+	/**
+	*	Reads an unsigned short.
+	*/
+	int ReadWord();
+
+	/**
+	*	Reads a signed long.
+	*/
+	int ReadLong();
+
+	/**
+	*	Reads a float.
+	*/
+	float ReadFloat();
+
+	/**
+	*	Reads a string.
+	*/
+	char* ReadString();
+
+	/**
+	*	Reads a coordinate.
+	*/
+	float ReadCoord();
+
+	/**
+	*	Reads an angle.
+	*/
+	float ReadAngle();
+
+	/**
+	*	Reads a high resolution angle.
+	*/
+	float ReadHiResAngle();
+};
+
+/**
+*	Write buffer.
+*/
+class BufferWriter : public CBaseBuffer
+{
+public:
+	using CBaseBuffer::CBaseBuffer;
+
+	/**
+	*	Writes an unsigned char.
+	*/
+	void WriteByte( unsigned char data );
+
+	/**
+	*	Writes a signed long.
+	*/
+	void WriteLong( int data );
+
+	/**
+	*	Writes a string.
+	*/
+	void WriteString( const char* pszString );
+};
 
 #endif // PARSEMSG_H
 
