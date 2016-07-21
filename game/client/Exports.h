e95d147b466ae6487552e6c406e2b450dbaa02f8
Sam <duo929@hotmail.com>
2016-07-21 20:43:16 +0200

    Tidy & doc Exports.h

--- game/client/Exports.h
+++ game/client/Exports.h
@@ -1,6 +1,30 @@
+/***
+*
+*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+#ifndef GAME_CLIENT_EXPORTS_H
+#define GAME_CLIENT_EXPORTS_H
+
 //CL_DLLEXPORT has been replaced with DLLEXPORT. Secure clients have been obsolete for a long time. - Solokiller
 
-//TODO: document these functions, figure out when they're called. - Solokiller
+/**
+*	@defgroup ClientLibExports Client Lib Exports
+*
+*	See cldll_func_t for the documentation for these functions. - Solokiller
+*	@see cldll_func_t
+*
+*	@{
+*/
 extern "C" 
 {
 	// From hl_weapons
@@ -66,3 +90,7 @@ extern "C"
 	// From GameStudioModelRenderer
 	int DLLEXPORT HUD_GetStudioModelInterface( int version, struct r_studio_interface_s **ppinterface, struct engine_studio_api_s *pstudio );
 }
+
+/** @} */
+
+#endif //GAME_CLIENT_EXPORTS_H
