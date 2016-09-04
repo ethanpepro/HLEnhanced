fd21a89d5666ff3d5567cfc7945ba5edfd869aed
Sam <duo929@hotmail.com>
2016-09-04 18:05:35 +0200

    Make client view vectors extern in view.h

--- game/client/renderer/view.h
+++ game/client/renderer/view.h
@@ -9,6 +9,13 @@
 #define VIEWH 
 #pragma once
 
+extern Vector v_origin;
+extern Vector v_angles;
+extern Vector v_cl_angles;
+extern Vector v_sim_org;
+extern Vector v_lastAngles;
+extern Vector v_client_aimangles;
+
 void V_StartPitchDrift( void );
 void V_StopPitchDrift( void );
 
