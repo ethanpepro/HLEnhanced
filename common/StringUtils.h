d0d5b2a7a31020b6225fe2d0410b9da6f2670984
Sam <duo929@hotmail.com>
2016-10-28 13:35:08 +0200

    Add string trim helper functions

--- common/StringUtils.h
+++ common/StringUtils.h
@@ -1,6 +1,7 @@
 #ifndef COMMON_STRINGUTILS_H
 #define COMMON_STRINGUTILS_H
 
+#include <algorithm>
 #include <cctype>
 #include <cstring>
 #include <functional>
@@ -137,5 +138,34 @@ bool UTIL_TokenMatches( const char* pszString, const char* pszToken );
 char* UTIL_FixSlashes( char* pszPath );
 
 char* UTIL_SafeStrncpy( char* pszDest, const char* pszSource, const size_t uiSizeInCharacters );
+
+/*
+*	This code is based on the following Stack Overflow answer: http://stackoverflow.com/a/217605
+*/
+// trim from start
+inline std::string& LTrim( std::string& s )
+{
+	s.erase( s.begin(), std::find_if( s.begin(), s.end(),
+									  std::not1( std::ptr_fun<int, int>( std::isspace ) ) ) );
+	return s;
+}
+
+// trim from end
+inline std::string& RTrim( std::string& s )
+{
+	s.erase( std::find_if( s.rbegin(), s.rend(),
+						   std::not1( std::ptr_fun<int, int>( std::isspace ) ) ).base(), s.end() );
+	return s;
+}
+
+// trim from both ends
+inline std::string& Trim( std::string& s )
+{
+	return LTrim( RTrim( s ) );
+}
+
+/*
+*	End based on code.
+*/
 
 #endif //COMMON_STRINGUTILS_H
