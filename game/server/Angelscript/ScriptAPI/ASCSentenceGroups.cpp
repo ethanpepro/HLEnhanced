6e0e71e69337f597b80319c5417e138f7122c0c8
Sam <duo929@hotmail.com>
2016-11-07 18:01:48 +0100

    Tidy AS sentence groups API implementation

--- game/server/Angelscript/ScriptAPI/ASCSentenceGroups.cpp
+++ game/server/Angelscript/ScriptAPI/ASCSentenceGroups.cpp
@@ -6,39 +6,26 @@
 
 #include "ASCSentenceGroups.h"
 
-//TODO: remove helpers for non-string functions
-static void CSentenceGroups_Stop( enginefuncs_t* pThis, 
-								  CBaseEntity* pEntity, int iSentenceGroup, int iPick )
-{
-	g_Sentences.Stop( pEntity, iSentenceGroup, iPick );
-}
-
-static int CSentenceGroups_PlayRandomIndex( enginefuncs_t* pThis,
-											CBaseEntity* pEntity, int iSentenceGroup, float flVolume, float flAttenuation, int iFlags, int iPitch )
-{
-	return g_Sentences.PlayRndI( pEntity, iSentenceGroup, flVolume, flAttenuation, iFlags, iPitch );
-}
-
-static int CSentenceGroups_PlayRandomGroup( enginefuncs_t* pThis,
+static int CSentenceGroups_PlayRandomGroup( CSentenceGroups* pThis,
 											CBaseEntity* pEntity, const std::string& szGroupName, float flVolume, float flAttenuation, int iFlags, int iPitch )
 {
 	return g_Sentences.PlayRndSz( pEntity, szGroupName.c_str(), flVolume, flAttenuation, iFlags, iPitch );
 }
 
-static int CSentenceGroups_PlaySequentialGroup( enginefuncs_t* pThis, 
+static int CSentenceGroups_PlaySequentialGroup( CSentenceGroups* pThis,
 												CBaseEntity* pEntity, const std::string& szGroupName, 
 												float flVolume, float flAttenuation, int iFlags, int iPitch, int iPick, const bool bReset )
 {
 	return g_Sentences.PlaySequentialSz( pEntity, szGroupName.c_str(), flVolume, flAttenuation, iFlags, iPitch, iPick, bReset );
 }
 
-static int CSentenceGroups_GetIndex( enginefuncs_t* pThis,
+static int CSentenceGroups_GetIndex( CSentenceGroups* pThis,
 									 const std::string& szGroupName )
 {
 	return g_Sentences.GetIndex( szGroupName.c_str() );
 }
 
-//TODO: SENTENCEG_Lookup? - Solokiller
+//TODO: CSentenceGroups::Lookup? - Solokiller
 
 #define AS_CSENTENCEGROUPS_NAME "CSentenceGroups"
 
@@ -53,11 +40,11 @@ void RegisterScriptCSentenceGroups( asIScriptEngine& engine )
 
 	engine.RegisterObjectMethod(
 		pszObjectName, "void Stop(CBaseEntity@ pEntity, int iSentenceGroup, int iPick)",
-		asFUNCTION( CSentenceGroups_Stop ), asCALL_CDECL_OBJFIRST );
+		asMETHOD( CSentenceGroups, Stop ), asCALL_THISCALL );
 
 	engine.RegisterObjectMethod(
 		pszObjectName, "int PlayRandomIndex(CBaseEntity@ pEntity, int iSentenceGroup, float flVolume, float flAttenuation, int iFlags, int iPitch)",
-		asFUNCTION( CSentenceGroups_PlayRandomIndex ), asCALL_CDECL_OBJFIRST );
+		asMETHOD( CSentenceGroups, PlayRndI ), asCALL_THISCALL );
 
 	engine.RegisterObjectMethod(
 		pszObjectName, "int PlayRandomGroup(CBaseEntity@ pEntity, const string& in szGroupName, float flVolume, float flAttenuation, int iFlags, int iPitch)",
@@ -73,5 +60,5 @@ void RegisterScriptCSentenceGroups( asIScriptEngine& engine )
 		asFUNCTION( CSentenceGroups_GetIndex ), asCALL_CDECL_OBJFIRST );
 
 	engine.RegisterGlobalProperty( 
-		AS_CSENTENCEGROUPS_NAME " SentenceGroups", &g_engfuncs );
+		AS_CSENTENCEGROUPS_NAME " Sentences", &g_Sentences );
 }
