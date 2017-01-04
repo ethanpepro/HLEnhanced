c7cad36e45257b0b8506d949e2d1858030339ee6
Sam <duo929@hotmail.com>
2017-01-04 15:02:57 +0100

    Fix preprocessor statement, merge Source 2013 dllexport macros with current macros

--- source_sdk/public/tier0/platform.h
+++ source_sdk/public/tier0/platform.h
@@ -280,7 +280,7 @@ typedef void * HINSTANCE;
 #elif _LINUX
 	#define ALIGN16 __attribute__((aligned(16)))
 	#define ALIGN32 __attribute__((aligned(32)))
-#elif
+#else
 	#define ALIGN16 /* */
 	#define ALIGN32 /* */
 #endif
@@ -293,32 +293,37 @@ typedef void * HINSTANCE;
 	#define SELECTANY static
 #endif
 
-#if defined(_WIN32) && !defined(_XBOX)
+//Combined Source 2013 version of this with the Source 2006 version for OSX support. - Solokiller
+#if defined( _WIN32 ) && !defined(_XBOX)
 
 // Used for dll exporting and importing
-#define  DLL_EXPORT   extern "C" __declspec( dllexport ) 
-#define  DLL_IMPORT   extern "C" __declspec( dllimport )
+#define DLL_EXPORT				extern "C" __declspec( dllexport )
+#define DLL_IMPORT				extern "C" __declspec( dllimport )
 
 // Can't use extern "C" when DLL exporting a class
-#define  DLL_CLASS_EXPORT   __declspec( dllexport ) 
-#define  DLL_CLASS_IMPORT   __declspec( dllimport )
+#define DLL_CLASS_EXPORT		__declspec( dllexport )
+#define DLL_CLASS_IMPORT		__declspec( dllimport )
 
 // Can't use extern "C" when DLL exporting a global
-#define  DLL_GLOBAL_EXPORT   extern __declspec( dllexport ) 
-#define  DLL_GLOBAL_IMPORT   extern __declspec( dllimport )
+#define DLL_GLOBAL_EXPORT		extern __declspec( dllexport )
+#define DLL_GLOBAL_IMPORT		extern __declspec( dllimport )
 
-#elif defined _LINUX
+#define DLL_LOCAL
+
+#elif defined GNUC
 // Used for dll exporting and importing
-#define  DLL_EXPORT   extern "C" 
-#define  DLL_IMPORT   extern "C" 
+#define  DLL_EXPORT   extern "C" __attribute__ ((visibility("default")))
+#define  DLL_IMPORT   extern "C"
 
 // Can't use extern "C" when DLL exporting a class
-#define  DLL_CLASS_EXPORT   
-#define  DLL_CLASS_IMPORT  
+#define  DLL_CLASS_EXPORT __attribute__ ((visibility("default")))
+#define  DLL_CLASS_IMPORT
 
 // Can't use extern "C" when DLL exporting a global
-#define  DLL_GLOBAL_EXPORT   extern
-#define  DLL_GLOBAL_IMPORT   extern 
+#define  DLL_GLOBAL_EXPORT   extern __attribute ((visibility("default")))
+#define  DLL_GLOBAL_IMPORT   extern
+
+#define  DLL_LOCAL __attribute__ ((visibility("hidden")))
 
 #elif defined(_XBOX)
 
