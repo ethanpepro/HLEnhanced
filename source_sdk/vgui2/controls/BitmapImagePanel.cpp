ce1c43411c04b30a95dc9d16e26c86265542d88d
Sam <duo929@hotmail.com>
2017-01-05 15:59:39 +0100

    FIx implicit conversion from int to bool

--- source_sdk/vgui2/controls/BitmapImagePanel.cpp
+++ source_sdk/vgui2/controls/BitmapImagePanel.cpp
@@ -50,7 +50,7 @@ CBitmapImagePanel::CBitmapImagePanel( Panel *parent, char const *panelName,
 
 	if ( filename && filename[ 0 ] )
 	{
-		m_pImage = scheme()->GetImage( filename, NULL );
+		m_pImage = scheme()->GetImage( filename, false );
 		m_pszImageName = CloneString( filename );
 	}
 
